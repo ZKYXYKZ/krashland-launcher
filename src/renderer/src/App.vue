4884cc68d769f3c164b50354dc6a452490cd97e3
ZKYXYKZ <ludovic@wipier.fr>
2026-07-05 16:31:38 +0200

    Update

--- src/renderer/src/App.vue
+++ src/renderer/src/App.vue
@@ -12,8 +12,10 @@ import TitleBar from '@/components/TitleBar.vue'
 import MainView from '@/views/MainView.vue'
 import UpdateBanner from '@/components/UpdateBanner.vue'
 
+import sakuraUrl from '@/assets/sakura.mp3'
+
 onMounted(() => {
-  const audio = new Audio('/sakura.mp3')
+  const audio = new Audio(sakuraUrl)
   audio.volume = 0.3
   audio.play().catch(() => {})
 })
