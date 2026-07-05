f187ac0cd9f4a46f0754521e255bc42f15468699
ZKYXYKZ <ludovic@wipier.fr>
2026-07-05 16:27:26 +0200

    sakura

--- src/renderer/src/App.vue
+++ src/renderer/src/App.vue
@@ -7,7 +7,14 @@
 </template>
 
 <script setup>
+import { onMounted } from 'vue'
 import TitleBar from '@/components/TitleBar.vue'
 import MainView from '@/views/MainView.vue'
 import UpdateBanner from '@/components/UpdateBanner.vue'
+
+onMounted(() => {
+  const audio = new Audio('/sakura.mp3')
+  audio.volume = 0.3
+  audio.play().catch(() => {})
+})
 </script>
